4fa089c3c87d88a0ac3c5268f6013d9fbdf9ecd8
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2021-10-12 11:32:06 +0200

    Projektdateien hinzufügen.

---
+++ REST ML API/Controllers/ImageRecognitionController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_ML_API.Controllers
+{
+    #region model input class
+    public class ModelInput
+    {
+        [ColumnName(@"Label")]
+        public string Label { get; set; }
+
+        [ColumnName(@"ImageSource")]
+        public string ImageSource { get; set; }
+
+    }
+    #endregion
+
+    #region model output class
+    public class ModelOutput
+    {
+        [ColumnName("PredictedLabel")]
+        public string Prediction { get; set; }
+
+        public float[] Score { get; set; }
+    }
+    #endregion
+
+    [ApiController]
+    [Route("/api/images/recognition")]
+    public class ImageRecognitionController : Controller
+    {
+        public readonly Lazy<PredictionEngine<ModelInput, ModelOutput>> PredictionEngine = new Lazy<PredictionEngine<ModelInput, ModelOutput>>(() => CreatePredictionEngine(), true);
+
+        [HttpPost]
+        public Prediction Post([FromForm] IFormFile image)
+        {
+            var filePath = Path.GetTempFileName();
+
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                image.CopyToAsync(stream).Wait();
+            }
+
+            ModelInput sampleData = new ModelInput()
+            {
+                ImageSource = filePath,
+            };
+
+            var predEngine = PredictionEngine.Value;
+
+            var predictionResult = predEngine.Predict(sampleData);
+
+            return new Prediction() { Number = predictionResult.Prediction, Propability = String.Join(",", predictionResult.Score) };
+        }
+
+        private static PredictionEngine<ModelInput, ModelOutput> CreatePredictionEngine()
+        {
+            var mlContext = new MLContext();
+            ITransformer mlModel = mlContext.Model.Load(Path.GetFullPath("MLModel.zip"), out var _);
+            return mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+        }
+    }
+
+}
---
+++ REST ML API/Prediction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_ML_API
+{
+    public class Prediction
+    {
+        public string Number { get; set; }
+        public string Propability { get; set; }
+    }
+}
